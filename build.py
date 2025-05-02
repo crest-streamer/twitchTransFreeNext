026452dc9bc92a26ebc8faea3da937dae31a3202
crest <81942713+crest-streamer@users.noreply.github.com>
2025-05-03 08:02:36 +0900

    Update build.py

--- build.py
+++ build.py
@@ -135,11 +135,11 @@ def main(target_os):
 
     # 各OS向けにビルド
     if target_os == "windows":
-        build_for_os("windows", "", "--add-data=cacert.pem;.")
+        build_for_os("windows", "", "--include-data-file==cacert.pem=cacert.pem")
     elif target_os == "linux":
-        build_for_os("linux", "", "--add-data=cacert.pem:.")
+        build_for_os("linux", "", "--include-data-file==cacert.pem=cacert.pem")
     elif target_os == "macos_M1" or target_os == "macos_Intel":
-        add_data_option = "--add-data=cacert.pem:."
+        add_data_option = "---include-data-file==cacert.pem=cacert.pem"
         if target_os == "macos_M1":
             build_for_os("macos", "arm64", add_data_option)
         else:
